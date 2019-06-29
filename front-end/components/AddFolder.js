167b37a8baec10418bb673d4d4283be24182ce2a
Md Asaduzzaman <asad@aadibd.com>
2019-06-29 19:35:13 +0600

    WIP: add folder

--- front-end/components/AddFolder.js
+++ front-end/components/AddFolder.js
@@ -4,42 +4,113 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import {makeStyles} from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
+
+import DoneIcon from '@material-ui/icons/Done'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 
 const FormDialog = (props) => {
+    const colors = [
+        '#f44336',
+        '#e91e63',
+        '#9c27b0',
+        '#673ab7',
+        '#3f51b5',
+        '#2196f3',
+        '#03a9f4',
+        '#00bcd4',
+        '#009688',
+        '#4caf50',
+        '#ffc107',
+        '#ff9800'
+    ];
+
+
+    const [state, setState] = React.useState({
+        checked:[]
+    });
+
+    const handleChange = name => event => {
+        console.log(name);
+        console.log(state.checked);
+        setState({checked:[]});
+        setState({checked: {[name]:event.target.checked}});
+    };
+
     function handleClose() {
         return props.handleClose();
     }
 
+    function f() {
+
+        return colors.map(color => {
+            const useStyles = makeStyles(theme => ({
+                color: {
+                    backgroundColor: color,
+                    margin: 5,
+                    width: 48,
+                    height: 48,
+                    borderRadius:0,
+                    '&:hover': {
+                        backgroundColor: `${color}!important`,
+                    }
+                },
+                icon: {
+                    color: "white",
+                    fontSize:30
+                }
+            }));
+            const classes = useStyles();
+            return (
+                <Tooltip title={color} aria-label={color} placement="right">
+                    <Radio className={classes.color}
+                           checkedIcon={<DoneIcon className={classes.icon}/>}
+                           icon=""
+                           name="color"
+                        // checked={state.checked[color]}
+                        //    onChange={handleChange({color})}
+                           value={color}
+                           inputProps={{
+                               'aria-label': 'primary checkbox',
+                           }}
+                    />
+                </Tooltip>)
+        })
+    }
+
+
     return (
         <div>
-            {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Open form dialog
-      </Button> */}
-            <Dialog open={props.open} onClose={handleClose} aria-labelledby="customized-dialog-title">
-                <DialogTitle id="customized-dialog-title">Subscribe</DialogTitle>
-                <DialogContent dividers>
-                    <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We will send updates
-                        occasionally.
-          </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Folder name"
-                        type="text"
-                        fullWidth
-                    />
+            <Dialog
+                open={props.open} onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                TransitionComponent={Transition}
+                keepMounted
+                fullWidth={true}
+                color="inherit"
+                maxWidth={'sm'}>
+                <DialogTitle id="customized-dialog-title">Add New Folder</DialogTitle>
+                <DialogContent dividers fullwidth>
+                    <TextField autoFocus margin="dense" id="name" label="Folder name" type="text" fullWidth/>
+                    {f()}
+
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
                         Cancel
-          </Button>
+                    </Button>
                     <Button onClick={handleClose} color="primary">
-                        Subscribe
-          </Button>
+                        Add
+                    </Button>
                 </DialogActions>
             </Dialog>
         </div>
--- front-end/components/Folder.js
+++ front-end/components/Folder.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from '../routes'
-import { Button, Box, Grid, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Folder as FolderIcon } from '@material-ui/icons'
 
@@ -12,11 +11,11 @@ export default function Folder(props) {
             textDecoration: 'none',
             display: 'flex',
             alignItems: 'center',
-            color: props.folder.color,
+            color: props.folder.color?props.folder.color: theme.palette.primary.main,
             transition: 'all 0.3s ease-out',
             borderRadius: 4,
             '&:hover': {
-                backgroundColor: props.folder.color,
+                backgroundColor: props.folder.color?props.folder.color: theme.palette.primary.main,
                 color: '#fff',
             }
         },
@@ -25,7 +24,6 @@ export default function Folder(props) {
         }
     }));
     const classes = useStyles();
-    // return <div> <FolderIcon className={classes.driveFolderIcon} /> <span>{props.folder.name}</span></div>
     return <Link route='folder' params={{ slug: props.folder.slug }}>
         <a className={classes.folder}>
             <FolderIcon className={classes.driveFolderIcon} /> <span>{props.folder.name}</span>
